ed8868f484351b078cacb0249e59333871f06216
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-12-27 00:12:52 +0100

    Remove old configure-mouse script

--- bin/configure-mouse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Touchpad settings are configurable in /etc/X11/xorg.conf.d/*-synaptics.conf
-
-# Disable the touchpad if any device named mouse is detected by xinput
-xinput --list | egrep -i 'mouse|receiver' &> /dev/null
-if [ $? -eq 0 ]; then
-  synclient TouchpadOff=1
-fi
